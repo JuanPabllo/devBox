1a3e5839fb56ea3a8f8a91e66b14e19f24231edb
Juan Pablo <juanpablo.codes@gmail.com>
2021-09-08 22:39:19 -0300

    test for func progress upload

--- backend/test/unit/uploadHandler.test.js
+++ backend/test/unit/uploadHandler.test.js
@@ -7,6 +7,7 @@ import TestUtil from '../_utils/testUtil';
 import { logger } from '../../src/logger';
 
 import UploadHandler from './../../src/uploadHandler';
+import testUtil from '../_utils/testUtil';
 
 describe('#UploadHandler test suite', () => {
   const ioObj = {
@@ -86,6 +87,31 @@ describe('#UploadHandler test suite', () => {
         params.filename
       );
       expect(fs.createWriteStream).toHaveBeenCalledWith(expectedFileName);
+    });
+  });
+
+  describe('#handlerFileBytes', () => {
+    test('should call emit function and it is a tranform string', async () => {
+      jest.spyOn(ioObj, ioObj.to.name);
+      jest.spyOn(ioObj, ioObj.emit.name);
+
+      const handler = new UploadHandler({
+        io: ioObj,
+        socketId: '01',
+      });
+
+      const messages = ['hello'];
+      const source = testUtil.generateReadableStream(messages);
+      const onWrite = jest.fn();
+      const target = testUtil.generateWritableStream(onWrite);
+
+      await pipeline(source, handler.handlerFileBytes('fileName.txt'), target);
+
+      expect(ioObj.to).toHaveBeenCalledTimes(messages.length);
+      expect(ioObj.emit).toHaveBeenCalledTimes(messages.length);
+
+      expect(onWrite).toHaveBeenCalledTimes(messages.length);
+      expect(onWrite.mock.calls.join()).toEqual(messages.join());
     });
   });
 });
